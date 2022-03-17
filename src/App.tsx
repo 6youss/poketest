cf549410ab5e99d8cb8c9b5ce0af935df5982144
Youcef ZAHARIOU <youcef.zahariou@gmail.com>
2022-03-17 14:49:36 +0100

    ♻️ add StatusBar options

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Router} from './navigation/Router';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StatusBar} from 'react-native';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <>
+      <StatusBar translucent backgroundColor={'transparent'} />
       <SafeAreaProvider>
         <QueryClientProvider client={queryClient}>
           <NavigationContainer>
